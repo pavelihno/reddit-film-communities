978d6eb763e9c419238624f57d1444113a4afefb
Juan Baldelomar <juan.baldelomar@cimat.mx>
2026-01-06 11:43:14 +0100

    Added movie of origin to each comment.

--- utils/id_extractor.py
+++ utils/id_extractor.py
@@ -14,9 +14,18 @@ def get_ids_list(file_path: str) -> list[str]:
     soup = BeautifulSoup(movies_content, 'html.parser')
 
     ids = []
-    for link in soup.find_all("a"):
-        link_text = link.get("href")
-        if "reddit" in link_text:
-            ids.append(extract_post_id(link_text))
+    current_movie = ""
+    for ol in soup.find_all("ol"):
+        li = ol.find_next("li")
+        if li:
+            span = li.find_next("span")
+            if span and span.get("class", [""])[0] == "c5":
+                current_movie = span.text
+                continue
+
+        for link in ol.find_all("a"):
+            link_text = link.get("href")
+            if "reddit" in link_text:
+                ids.append((extract_post_id(link_text), current_movie))
 
     return ids
